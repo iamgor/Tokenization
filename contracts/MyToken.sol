3f1ecb28b692eaec6112c39fc2bf97524564ba86
Igor A <aiv_igor@yahoo.com>
2021-10-11 06:55:27 +0300

    Added openzeppelin & update my token

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
-contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("StarDucks Cappucino Token", "CAPPU", 0) {
+contract MyToken is ERC20Upgradeable {
+    constructor(uint256 initialSupply)
+        ERC20("StarDucks Cappucino Token", "CAPPU", 0)
+    {
         _mint(msg.sender, initialSupply);
     }
 }
